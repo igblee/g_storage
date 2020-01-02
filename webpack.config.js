bd3182280c92ec4b25cf60972ae9014c8cd40d8d
glenn <1812879822@qq.com>
2020-01-02 21:56:03 +0800

    chore: build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
 const path = require('path');
-const library = 'storage';
-module.exports = {
+const shellJs = require('shelljs')
+const merge = require('webpack-merge')
+const fs = require('fs')
+shellJs.exec('rm -rf dist/*')
+const base = {
   mode: 'production',
   entry: path.join(__dirname, 'index.ts'),
-  output: {
-    library: {
-      root: library,
-      amd: `${library}.amd`,
-      commonjs: `${library}.common`
-    },
-    libraryTarget: 'umd',
-    path: path.join(__dirname, 'dist'),
-    filename: 'main.js',
-    globalObject: 'this'
+  optimization: {
+    minimize: true,
+    removeAvailableModules: true,
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: "commons",
+          chunks: "initial"
+        }
+      }
+    }
   },
+  externals: {
+    'crypto-js': {
+        commonjs: 'crypto-js',
+        commonjs2: 'crypto-js',
+        amd: 'crypto-js',
+        root: 'cryptoJs'
+    }
+  },
+  target: 'web',
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
   module: {
     rules: [{
       test: /\.tsx?$/,
@@ -32,6 +47,50 @@ module.exports = {
   },
   resolve: {
     extensions: ['.json', '.js', '.jsx', '.ts', '.tsx']
-  },
-  devtool: 'cheap-module-eval-source-map',
+  }
 };
+
+module.exports = [
+  merge(base, {
+    output: {
+      filename: './storage-amd.js',
+      path: path.join(__dirname, 'dist'),
+      libraryTarget: 'amd',
+      globalObject: 'this'
+
+    }
+  }),
+  merge(base, {
+    output: {
+      filename: './storage-umd.js',
+      path: path.join(__dirname, 'dist'),
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+      globalObject: 'typeof self !== \'undefined\' ? self : this'
+    }
+  }),
+  merge(base, {
+    output: {
+      filename: './storage-commonjs.js',
+      path: path.join(__dirname, 'dist'),
+      libraryTarget: 'commonjs',
+      globalObject: 'this'
+    }
+  }),
+  merge(base, {
+    output: {
+      filename: './storage-commonjs2.js',
+      path: path.join(__dirname, 'dist'),
+      libraryTarget: 'commonjs2',
+      globalObject: 'this'
+    }
+  }),
+  merge(base, {
+    output: {
+      filename: './storage-esm.js',
+      path: path.join(__dirname, 'dist'),
+      libraryTarget: 'commonjs-module',
+      globalObject: 'this'
+    }
+  })
+]
